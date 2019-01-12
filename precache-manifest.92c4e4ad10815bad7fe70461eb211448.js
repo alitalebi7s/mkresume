94e36955e61c6b381b41930bf73d533e877d3806
aliTalebi <talebi.ali.prv@gmail.com>
2019-01-12 21:07:35 +0330

    Updates

---
+++ precache-manifest.92c4e4ad10815bad7fe70461eb211448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "faf15ef4a6f1531b30cb",
+    "url": "/mkresume/static/js/runtime~main.faf15ef4.js"
+  },
+  {
+    "revision": "2aacab73846dded52429",
+    "url": "/mkresume/static/js/main.2aacab73.chunk.js"
+  },
+  {
+    "revision": "b668a280872bdd81bbfa",
+    "url": "/mkresume/static/js/1.b668a280.chunk.js"
+  },
+  {
+    "revision": "2aacab73846dded52429",
+    "url": "/mkresume/static/css/main.7c1cfbd2.chunk.css"
+  },
+  {
+    "revision": "3588145b271f3b99cb059f3abb6b9e03",
+    "url": "/mkresume/index.html"
+  }
+];
