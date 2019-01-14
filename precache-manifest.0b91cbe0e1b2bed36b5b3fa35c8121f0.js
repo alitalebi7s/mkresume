f5d6f9a0779a29cae00bd0a5e37d8ab608ca8f57
aliTalebi <talebi.ali.prv@gmail.com>
2019-01-14 11:46:48 +0330

    Updates

---
+++ precache-manifest.0b91cbe0e1b2bed36b5b3fa35c8121f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "faf15ef4a6f1531b30cb",
+    "url": "/mkresume/static/js/runtime~main.faf15ef4.js"
+  },
+  {
+    "revision": "7caf02bf70b07493075c",
+    "url": "/mkresume/static/js/main.7caf02bf.chunk.js"
+  },
+  {
+    "revision": "23852f1bcdfa437e0d94",
+    "url": "/mkresume/static/js/1.23852f1b.chunk.js"
+  },
+  {
+    "revision": "7caf02bf70b07493075c",
+    "url": "/mkresume/static/css/main.4bdae9ab.chunk.css"
+  },
+  {
+    "revision": "c1852deed4d2f917a505d471d0f031c5",
+    "url": "/mkresume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mkresume/precache-manifest.4f31fddd612d121d4fac347fcb151534.js"
+  "/mkresume/precache-manifest.0b91cbe0e1b2bed36b5b3fa35c8121f0.js"
 );
 
 workbox.clientsClaim();
